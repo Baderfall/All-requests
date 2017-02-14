6acf91bc6a7c1bbea4f9d984ee6147331c44ede6
Aliaksei Yarmash <Aliaksei_Yarmash@epam.com>
2017-02-14 16:04:24 +0300

    add promises

--- index.js
+++ index.js
@@ -3,7 +3,13 @@ const btn = document.querySelector('#search-button');
 
 btn.addEventListener('click', () => {
   const value = field.value;
-  getPhotoSrc(value);
+  drawPhoto(value);
+});
+
+field.addEventListener('keydown', (e) => {
+  if (e.keyCode === 13) {
+    btn.click();
+  }
 });
 
 function getSrc(response) {
@@ -12,13 +18,13 @@ function getSrc(response) {
   return src;
 }
 
-function getImage(src) {
-  var img = document.createElement('img');
+function getImg(src) {
+  const img = document.createElement('img');
   img.src = src;
   return img;
 }
 
-function drawImage(img) {
+function drawImg(img) {
   const wrapper = document.querySelector('.img-wrapper');
   wrapper.innerHTML = '';
   wrapper.appendChild(img);
@@ -26,25 +32,99 @@ function drawImage(img) {
 
 /******CLASSIC AJAX / COMET*******/
 
-function getPhotoSrc(value) {
+function drawPhoto(value) {
   const xhr = new XMLHttpRequest();
-  const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyB9COzxg7qOvThJlJcRb523mybrL9MSU84&type=video&part=snippet&maxResults=15&q=${value}`;
+  const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyB9COzxg7qOvThJlJcRb523mybrL9MSU84&type=video&part=snippet&maxResults=1&q=${value}`;
+  
   xhr.open('GET', url);
-  xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
   xhr.send();
-
-  xhr.onreadystatechange = function() { // (3)
+  xhr.onreadystatechange = function() {
     if (xhr.readyState !== 4) {
       return;
     }
   
-    if (xhr.status !== 200) {
-    } else {
+    if (xhr.status === 200) {
       const response = xhr.responseText;
       const src = getSrc(response);
-      const img = getImage(src);
-      drawImage(img);
+      const img = getImg(src);
+      
+      drawImg(img);
     }
   };
 }
 
+/*******AJAX / COMET onload onerror*******/
+
+function drawPhoto(value) {
+  const xhr = new XMLHttpRequest();
+  const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyB9COzxg7qOvThJlJcRb523mybrL9MSU84&type=video&part=snippet&maxResults=1&q=${value}`;
+  
+  xhr.open('GET', url);
+  xhr.send();
+  xhr.onload = function() {
+    const response = xhr.responseText;
+    const src = getSrc(response);
+    const img = getImg(src);
+    
+    drawImg(img);
+  }
+
+  xhr.onerror = function() {
+    console.log(xhr.statusText);
+  }
+}
+
+/******WEBSOCKET*****/
+
+/*const ws = new WebSocket('ws://echo.websocket.org');
+
+ws.onopen = function() {
+  console.log('Соединение установлено.');
+  this.send('ALO');
+};
+
+ws.onclose = function(e) {
+  if (e.wasClean) {
+    console.log('Соединение закрыто.');
+  } else {
+    console.log('Соединение разорвано.');
+  }
+  console.log('Код: ' + event.code + ' причина: ' + event.reason);
+};
+
+ws.onmessage = function(e) {
+  console.log('Получены данные: ' + e.data);
+};
+
+ws.onerror = function(error) {
+  console.log('Ошибка: ' + error.message);
+};
+
+setTimeout(function() {
+  ws.close();
+}, 3000);*/
+
+/*********PROMISE********/
+
+function drawPhoto(value) {
+  const promise = new Promise(function(resolve, reject) {
+    const xhr = new XMLHttpRequest();
+    const url = `https://www.googleapis.com/youtube/v3/search?key=AIzaSyB9COzxg7qOvThJlJcRb523mybrL9MSU84&type=video&part=snippet&maxResults=1&q=${value}`;
+    
+    xhr.open('GET', url);
+    xhr.send();
+    xhr.onload = function() {
+      resolve(this.responseText);
+    };
+  });
+  
+  promise.then(response => {
+    return getSrc(response);
+  })
+  .then(src => {
+    return getImg(src);
+  })
+  .then(img => {
+    drawImg(img);
+  });
+}
